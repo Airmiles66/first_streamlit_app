8825c6b2ce36b59ea5ec92bd1dd51cc2b3e8b0c9
Airmiles66 <36163373+Airmiles66@users.noreply.github.com>
2022-08-29 11:52:30 +0200

    Update streamlit_app.py
    Added new header for smootihie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('\N{pot of food} Omega 3 & Blueberry Oatmeal')
 streamlit.text('\N{broccoli} Kale, Spinach & Rocket Smoothie')
 streamlit.text('\N{egg} Hard-Boiled Free-Range Egg')
 
+streamlit.text('\N{banana} \N{strawberry} Build your own Fruit Smoothie')
+
