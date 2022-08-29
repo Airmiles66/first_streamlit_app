b946b5f6ae3a09f3b58aab1589f6db36860e7681
Airmiles66 <36163373+Airmiles66@users.noreply.github.com>
2022-08-29 11:45:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My first streamlit app is here')
 
-streamlit.header(\N {pot} 'Breakfast Favourites')
+streamlit.header('\N {pot} Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
