35a16dc30adcc4d2790944466509c9a9d11f3fca
Airmiles66 <36163373+Airmiles66@users.noreply.github.com>
2022-08-29 11:42:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My first streamlit app is here')
 
-streamlit.header(https://unicode.org/emoji/charts/emoji-list.html#1f372 'Breakfast Favourites')
+streamlit.header(#1f372 'Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
